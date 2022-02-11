1651fb48b0b82f4c814f15988a4f49f935eed071
AnnaDaraha <98885826+AnnaDaraha@users.noreply.github.com>
2022-02-11 18:34:29 +0300

    Update Task1.java

--- Lessons/src/Task1.java
+++ Lessons/src/Task1.java
@@ -1,56 +1,54 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
-  class TestTask {
+class TestTask {
+    TestTask() {
+    }
 
     public static void main(String[] args) {
- // Task 1.3
-     Scanner scanner = new Scanner(System.in);
-          System.out.print("Введите первое целое число! ");
-        String number = scanner.next();
-        if (new Scanner(number).hasNextInt()){
-            int trueNumber = Integer.parseInt(number);
-            if (trueNumber % 2 == 0){
-                System.out.println("Это четное число! ");
-            }else {
-                System.out.println("Это нечетное чило! ");
-            }
+        Scanner scanner = new Scanner(System.in);
 
-        }else {
-            System.out.println("Это не число!");
-        }
-        System.out.print("Введите второе целое число! ");
-        String number2 = scanner.next();
-        if (new Scanner(number2).hasNextInt()){
-            int trueNumber = Integer.parseInt(number2);
-            if (trueNumber % 1 == 0){
+        while(true) {
+            System.out.print("Введите первое целое число! ");
+            String character = scanner.next();
+            if ((new Scanner(character)).hasNextInt()) {
+                int trueNumber = Integer.parseInt(character);
                 if (trueNumber % 2 == 0) {
                     System.out.println("Это четное число! ");
                 } else {
                     System.out.println("Это нечетное число! ");
                 }
-            }
-            else {
-                System.out.println("Это не целое число! ");
-            }
-        }
-        else {
-            System.out.println("Это не число! ");
-        }
-        System.out.print("Введите букву! ");
-        String character = scanner.next();
-        char letter = character.charAt(0);
-        if (Character.isDigit(letter)) {
-            System.out.println("Это не буква! ");
-        } else if (Character.isLetter(letter)) {
-            System.out.println("Вы ввели букву " + letter);
-        }
 
-        }
+                while(true) {
+                    System.out.print("Введите второе целое число! ");
+                    character = scanner.next();
+                    if ((new Scanner(character)).hasNextInt()) {
+                        trueNumber = Integer.parseInt(character);
+                        if (trueNumber % 2 == 0) {
+                            System.out.println("Это четное число! ");
+                        } else {
+                            System.out.println("Это нечетное число! ");
+                        }
 
+                        while(true) {
+                            while(true) {
+                                System.out.print("Введите букву! ");
+                                character = scanner.next();
+                                char letter = character.charAt(0);
+                                if (Character.isDigit(letter)) {
+                                    System.out.println("Это не буква! ");
+                                } else if (Character.isLetter(letter)) {
+                                    System.out.println("Вы ввели букву " + letter);
+                                    return;
+                                }
+                            }
+                        }
+                    }
+
+                    System.out.println("Это не целое число! ");
+                }
+            }
+
+            System.out.println("Это не целое число!");
+        }
     }
-
 }
-
-
-
-
